67d8233043baa82fea35f876d6a4cd911f75c690
Thai Tran <thai.tommy.tran@gmail.com>
2024-09-27 18:01:57 -0500

    Added Analytics component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
+import Analytics from "./components/Analytics";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Hero />
+      <Analytics />
     </div>
   );
 }
