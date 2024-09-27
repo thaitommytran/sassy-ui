0579711314614027b70bee230759c1b6b04a1671
Thai Tran <thai.tommy.tran@gmail.com>
2024-09-27 17:14:39 -0500

    Fixed title padding and height

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -24,7 +24,7 @@ const Navbar = () => {
         )}
       </div>
       <div className={nav ? "fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500" : "fixed left-[-100%]"}>
-        <h1 className="w-full text-3xl font-bold text-[#00df9a] m-4 py-4">
+        <h1 className="w-full text-3xl font-bold text-[#00df9a] px-4 py-[30px]">
           SaaSy UI.
         </h1>
         <ul className="uppercase p-4">
