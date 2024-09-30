6edb1d88ecae94f74e7326c54d3c6ced637ec048
Thai Tran <thai.tommy.tran@gmail.com>
2024-09-29 21:37:24 -0500

    Fixed padding for consistency

--- src/components/Analytics.jsx
+++ src/components/Analytics.jsx
@@ -6,7 +6,7 @@ const Analytics = () => {
     <div className="w-full bg-white py-16 px-4">
       <div className="max-w-[1240px] mx-auto grid md:grid-cols-2">
         <img className="w-[500px] mx-auto my-4" src={laptop} alt="laptop" />
-        <div className="flex flex-col justify-center">
+        <div className="flex flex-col justify-center p-4 md:pr-12">
           <p className="text-[#00df9a] font-bold">DATA ANALYTICS DASHBOARD</p>
           <h1 className="md:text-4xl sm:text-3xl text-2xl font-bold py-2">
             Manage Data Analytics Centrally
